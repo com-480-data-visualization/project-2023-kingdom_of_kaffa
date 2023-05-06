2224e709cf98cd3e1efd2f5bdb9434b68009bc17
Kamila Babayeva <kamifai14@gmail.com>
2023-05-06 19:17:34 +0200

    Draft version of the map

--- website/scripts/figure1/figure1.js
+++ website/scripts/figure1/figure1.js
@@ -1,57 +1,109 @@
 
-function setMap(width, height, datapull) {
-  console.log(datapull)
-    const margin = {top: 10, right: 30, bottom: 10, left: 30};
-    width = width - margin.left - margin.right;
-    height = height - margin.top - margin.bottom;
-    const projection = d3.geoMercator()
-                      .rotate([-11, 0])
-                      .scale(1)
-                      .translate([0, 0]);
-    const path = d3.geoPath().projection(projection);
-    const svg = d3.select('#figure1')
-                 .append('svg')
-                 .attr('viewBox', '0 0 300 300')
-                 .attr('preserveAspectRatio', 'xMidYMid')
-                 .style('max-width', 1200)
-                 .style('margin', 'auto')
-                 .style('display', 'flex');
 
-    const b = path.bounds(datapull),
-    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0]  [1]) / height),
-    t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-    projection.scale(s).translate(t);
-    svg.selectAll('path')
-      .data(datapull.features)
-      .enter()
-      .append('path')
-      .attr('d', path)
+function setSlider(width, height, data_map){
+  const data = Array.from({length: 7}, (_, i) =>new Date(1990 + i*5, 10, 3));
+  let dateFormat = d3.timeFormat("%Y");
+
+  let slider = d3
+  .sliderBottom()
+  .min(data[0])
+  .max(data[6])
+  .step(5000 * 60 * 60 * 24 * 365)
+  .width(width*0.95)
+  .tickValues(data)
+  .tickFormat(d3.timeFormat('%Y'))
+  .ticks(7)
+  .default(data[0])
+  .on('onchange', val => {
+    transitionMap(data_map, (dateFormat(val)-1990)/5)
+  })
+
+  const g = d3.select('#map_slider_figure1')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(30,30)');
+  
+  g.call(slider);
+
+}
+
+function setMap(width, height, geojson) {
+  
+  // Select map svg and set size
+  const svg = d3.select("#svg_map_figure1")
+  .attr("width", width)
+  .attr("height", height)
+
+  let mouseOver = function(d) {
+    d3.selectAll(".Country")
+      .transition()
+      // .duration(200)
+      .style("opacity", .5)
+    d3.select(this)
+      .transition()
+      // .duration(200)
+      .style("opacity", 1)
+      // .style("stroke", "black")
   }
 
+  let mouseLeave = function(d) {
+    d3.selectAll(".Country")
+      .transition()
+      .duration(200)
+      .style("opacity", 1)
+    d3.select(this)
+      .transition()
+      .duration(200)
+      .style('stroke', '#fff')
+      .style('stroke-width', '1')
+  }
 
+  const projection = d3.geoMercator().scale(170).center([0,20]).translate([width/2,height/2])
 
+  const path = d3.geoPath().projection(projection);
+  const color_legend = d3.scaleLinear()
+        .domain([0, 3492660000])
+        .range(['#eacaae', '#643100'])
 
-      //   const svg = d3.select("svg"),
-      // width = +svg.attr("width"),
-      // height = +svg.attr("height");
+  svg.selectAll('path')
+    .data(geojson.features)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .style('fill', function(d) {
+      const value = d["properties"]['1990/91'];
+      if (value) {
+        return color_legend(d["properties"]['1990/91']);
+      } else {
+        return '#ccc';
+    }})
+    .style('stroke', '#fff')
+    .style('stroke-width', '1')
+      .attr("class", function(d){ return "Country" } )
+      .on("mouseover", mouseOver )
+      .on("mouseleave", mouseLeave )
+}
 
-      // // Map and projection
-      // const projection = d3.geoNaturalEarth1()
-      //     .scale(width / 1.3 / Math.PI)
-      //     .translate([width / 2, height / 2])
+function transitionMap(data, i) {
+  const color_legend = d3.scaleLinear()
+  .domain([0, 3492660000])
+  .range(['#eacaae', '#643100'])
 
-      //     const path = d3.geoPath().projection(projection);
+  const years = ["1990/91", "1995/96", "2000/01", "2005/06", "2010/11", "2015/16", "2019/20"] 
 
-
-
-      // // Load external data and boot
-      // d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then( function(data) {
-
-      //     // Draw the map
-      //     svg.selectAll('path')
-      //       .data(data.features)
-      //       .enter()
-      //     .append('path')
-      //       .attr('d', path)
-
-      // })
+  const svg = d3.select("#svg_map_figure1")
+  svg.selectAll('path')
+   .data(data.features)
+   .transition()
+   .delay(100)
+   .duration(1000)
+   .style('fill', function(d) {
+      const value = d["properties"][years[i]];
+      if (value) {
+        return color_legend(d["properties"][years[i]]);
+      } else {
+        return '#ccc';
+      }
+   })
+}
