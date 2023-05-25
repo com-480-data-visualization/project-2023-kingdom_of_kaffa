28309313d12704a3d29e3bd9e32052dbb3b04be6
Arist12 <1065543666@sjtu.edu.cn>
2023-05-25 22:23:03 +0200

    update fig 3

--- website/scripts/js/figure3.js
+++ website/scripts/js/figure3.js
@@ -1,48 +1,7 @@
-function add_legend(svg) {
-    // Create the legend
-    var legend = svg
-        .append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(40, 40)");
-
-    // Add a rectangle for each color and price range
-    legend
-        .selectAll("rect")
-        .data(["#5ac0f7", "#fdc62f", "#ea4242", "#ff8e8e"])
-        .enter()
-        .append("rect")
-        .attr("y", function (d, i) {
-            return i * 35;
-        })
-        .attr("x", 70)
-        .attr("width", 100)
-        .attr("height", 20)
-        .style("fill", function (d) {
-            return d;
-        });
-
-    // Add labels for each price range
-    legend
-        .selectAll("text")
-        .data([3, 3.5, 4, 4.5])
-        .enter()
-        .append("text")
-        .attr("y", function (d, i) {
-            return i * 40;
-        })
-        .attr("x", 40)
-        // .attr("dy", ".1em")
-        .text(function (d) {
-            return d;
-        });
-}
-
 $(document).ready(function () {
-    d3.csv("../dataset/kofio_dataset/price_rating_rec_clean.csv").then(
+    d3.csv("../dataset/kofio_dataset/coffee_items_filtering.csv").then(
         function (data) {
-            // make a copy of the original data points
             const record = JSON.parse(JSON.stringify(data));
-            // initialize the svg
             let width = window.innerWidth * 0.58,
                 height = window.innerHeight * 0.85,
                 radius = 30;
@@ -52,9 +11,7 @@ $(document).ready(function () {
                 .attr("height", height)
                 .attr("width", width);
 
-            // implement the filtering function
             function impl_filter(indicator) {
-                // remove all the viz first.
                 svg.selectAll(".circleContainer").remove();
                 svg.selectAll("circle").remove();
                 svg.selectAll("text").remove();
@@ -72,19 +29,48 @@ $(document).ready(function () {
                     .style("cursor", "pointer")
                     .attr("r", radius)
                     .style("fill", (d) => {
-                        if (d.Rating >= 4.5) return "#ff8e8e";
-                        else if (d.Rating >= 4) return "#ea4242";
-                        else if (d.Rating >= 3.5) return "#fdc62f";
-                        else return "#5ac0f7";
-                    })
-                    .on("click", function (d) {
-                        return 1;
+                        if (indicator === "fig3-show-all") {
+                            // Display bubbles using the existing color scheme
+                            return "#5ac0f7";
+                        } else if (indicator === "fig3-price") {
+                            if (d.Price <= 15) return "#ff8e8e";
+                            else if (d.Price <= 25) return "#ea4242";
+                            else return "#5ac0f7";
+                        } else if (indicator === "fig3-rating") {
+                            // Display bubbles based on flavor
+                            // Replace the following code with your custom logic
+                            if (d.Rating >= 4.5) return "#ff8e8e";
+                            else if (d.Rating >= 4) return "#ea4242";
+                            else if (d.Rating >= 3.5) return "#fdc62f";
+                            else return "#5ac0f7";
+                        } else if (indicator === "fig3-roast-type") {
+                            if (d["Roast Type"] === "Omni") return "#ea4242";
+                            else if (d["Roast Type"] === "Filter")
+                                return "#fdc62f";
+                            return "#0000ff"; // Blue color for roast type indicator
+                        } else if (indicator === "fig3-roast-level") {
+                            if (d["Roast Level"] === "Omni")
+                                return "#ffff00"; // Yellow color for brewing method indicator
+                            else if (
+                                d["Roast Level"] === "Light to Medium Light"
+                            )
+                                return "#fdc62f";
+                            return "#5ac0f7";
+                        }
                     });
-                add_legend(svg);
                 elem_updated
                     .append("text")
                     .attr("text-anchor", "middle")
-                    .text((d) => d.Rating);
+                    .style("font-size", "12px")
+                    .text((d) => {
+                        if (indicator === "fig3-show-all")
+                            return d["Item Name"].substring(0, 3);
+                        else if (indicator === "fig3-rating") return d.Rating;
+                        else if (indicator === "fig3-price") return d.Price;
+                        else if (indicator === "fig3-roast-type")
+                            return d["Roast Type"].substring(0, 3);
+                        else return d["Roast Level"].substring(0, 3);
+                    });
 
                 var simulation = d3
                     .forceSimulation()
@@ -108,14 +94,14 @@ $(document).ready(function () {
             d3.select("#fig3-price").on("click", function () {
                 impl_filter("fig3-price");
             });
-            d3.select("#fig3-flavor").on("click", function () {
-                impl_filter("fig3-flavor");
+            d3.select("#fig3-rating").on("click", function () {
+                impl_filter("fig3-rating");
             });
             d3.select("#fig3-roast-type").on("click", function () {
                 impl_filter("fig3-roast-type");
             });
-            d3.select("#fig3-brewing-method").on("click", function () {
-                impl_filter("fig3-brewing-method");
+            d3.select("#fig3-roast-level").on("click", function () {
+                impl_filter("fig3-roast-level");
             });
         }
     );
