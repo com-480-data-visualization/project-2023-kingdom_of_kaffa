661716e32e779e5ef7e991cb996d6b0e15e898bc
Fanfan2001 <1075443660@qq.com>
2023-05-26 11:38:11 +0200

    update homepage

---
+++ website/scripts/js/script.js
@@ -0,0 +1,21 @@
+
+const tl = gsap.timeline()
+
+tl.to('.stroke', {
+  duration: 2.5,
+  delay: 1.8,
+  strokeDashoffset: 0,
+})
+.to('.ripple', {
+  duration: 4,
+  delay: -2,
+  xPercent: -50,
+  yPercent: -60,
+})
+.to('#turbulence', {
+  duration: 1.5,
+  delay: -2,
+  attr: {
+    baseFrequency: '0 0',
+  },
+})
