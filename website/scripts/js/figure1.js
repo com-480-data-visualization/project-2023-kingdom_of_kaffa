d0b45cebd1d0ff7ce155c50d27f163221cb83066
Fanfan2001 <1075443660@qq.com>
2023-06-04 14:22:38 +0200

    update css

--- website/scripts/js/figure1.js
+++ website/scripts/js/figure1.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-    console.log(1);
     const element = document.querySelector("#fig1-svg");
     const width = element.offsetWidth;
 
-    const heightPercentage = 66;
+    const heightPercentage = 65.5;
     const height = (width * heightPercentage) / 100;
     element.style.height = height + "px";
+
+    var svgElement = document.querySelector('#viz');
+    svgElement.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    });
 });
